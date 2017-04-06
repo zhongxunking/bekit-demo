cc611899a1d05dab77e609b0c199aedc0cf983a6
zhongxun <24686421a>
2017-04-06 22:24:11 +0800

    创建Main类

---
+++ service-demo/src/main/java/demo/Main.java
@@ -0,0 +1,22 @@
+/* 
+ * 作者：钟勋 (e-mail:zhongxun@163.com)
+ */
+
+/*
+ * 修订记录:
+ * @author 钟勋 2017-04-06 22:15 创建
+ */
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
