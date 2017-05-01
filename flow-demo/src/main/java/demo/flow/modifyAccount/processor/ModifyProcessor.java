e9fb802bbfc5ec8f907599291005d1002168c431
zhongxun <24686421a>
2017-05-01 17:20:58 +0800

    增加日志

--- flow-demo/src/main/java/demo/flow/modifyAccount/processor/ModifyProcessor.java
+++ flow-demo/src/main/java/demo/flow/modifyAccount/processor/ModifyProcessor.java
@@ -39,10 +39,13 @@ public class ModifyProcessor {
         // 这里以随机数模拟调用账务系统的结果
         switch (RANDOM.nextInt(4)) {
             case 0:
+                logger.info("模拟账务系统返回成功");
                 return ResultStatus.SUCCESS;
             case 1:
+                logger.info("模拟账务系统返回失败");
                 return ResultStatus.FAIL;
             case 2:
+                logger.info("模拟账务系统返回处理中");
                 return ResultStatus.PROCESS;
             default:
                 throw new TimeoutException("模拟调用账务系统超时");
