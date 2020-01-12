374061b5403f924b7c95cb4029d42a57bd2c1303
zhongxunking <zhongxunking@163.com>
2020-01-12 12:01:32 +0800

    调整event-demo

--- event-demo/src/main/java/demo/listener/UserListener1.java
+++ event-demo/src/main/java/demo/listener/UserListener1.java
@@ -21,16 +21,21 @@ import org.bekit.event.listener.PriorityType;
 @DomainListener(priority = 1)
 @Slf4j
 public class UserListener1 {
+    // 监听AddUserEvent事件
+    // 结合@DomainListener(priority = 1)中priority属性，表示删除用户事件发生后本监听方法第一个执行
+    // 方法名可以任意取，这里取名为listenAddUserEvent是为了更贴合业务含义，你愿意的话取成abc也可以
     @Listen
-    public void listenAddUserEvent(AddUserEvent event) {    // 方法名可以任意取，这里取名为listenAddUserEvent是为了更贴合业务含义，你愿意的话取成abc也可以
-        // 这里为了展示方便，只打印了日志，在真实业务场景中你肯定还会做其他事:)
+    public void listenAddUserEvent(AddUserEvent event) {
         log.info("UserListener1监听到添加用户事件：{}", event);
+        // TODO 具体业务代码
     }
 
-    // @Listen的属性priorityType = PriorityType.DESC表示按照优先级倒叙排序，
+    // 监听DeleteUserEvent事件
+    // @Listen的属性priorityType = PriorityType.DESC表示按照优先级倒叙排序
     // 结合@DomainListener(priority = 1)中priority属性，表示删除用户事件发生后本监听方法倒数第一个执行
     @Listen(priorityType = PriorityType.DESC)
     public void listenDeleteUserEvent(DeleteUserEvent event) {
         log.info("UserListener1监听到删除用户事件：{}", event);
+        // TODO 具体业务代码
     }
 }
--- event-demo/src/main/java/demo/listener/UserListener2.java
+++ event-demo/src/main/java/demo/listener/UserListener2.java
@@ -16,21 +16,26 @@ import org.bekit.event.annotation.Listen;
 import org.bekit.event.listener.PriorityType;
 
 /**
- * 用户监听器2
+ * 用户监听器1
  */
 @DomainListener(priority = 2)
 @Slf4j
 public class UserListener2 {
+    // 监听AddUserEvent事件
+    // 结合@DomainListener(priority = 2)中priority属性，表示删除用户事件发生后本监听方法第二个执行
+    // 方法名可以任意取，这里取名为listenAddUserEvent是为了更贴合业务含义，你愿意的话取成abc也可以
     @Listen
-    public void listenAddUserEvent(AddUserEvent event) {    // 方法名可以任意取，这里取名为listenAddUserEvent是为了更贴合业务含义，你愿意的话取成abc也可以
-        // 这里为了方便展示，只打印了日志，在真实业务场景中你肯定会做其他事:)
+    public void listenAddUserEvent(AddUserEvent event) {
         log.info("UserListener2监听到添加用户事件：{}", event);
+        // TODO 具体业务代码
     }
 
-    // @Listen的属性priorityType = PriorityType.DESC表示按照优先级倒叙排序，
-    // 结合@BizListener(priority = 2)中priority属性，表示删除用户事件发生后本监听方法倒数第二个执行
+    // 监听DeleteUserEvent事件
+    // @Listen的属性priorityType = PriorityType.DESC表示按照优先级倒叙排序
+    // 结合@DomainListener(priority = 2)中priority属性，表示删除用户事件发生后本监听方法倒数第二个执行
     @Listen(priorityType = PriorityType.DESC)
     public void listenDeleteUserEvent(DeleteUserEvent event) {
         log.info("UserListener2监听到删除用户事件：{}", event);
+        // TODO 具体业务代码
     }
 }
